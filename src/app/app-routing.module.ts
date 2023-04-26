40a25cea074109ce6c07196d295da6997effcdda
Samir <ahmedsamir25999@gmail.com>
2023-04-26 03:42:16 +0200

    Day5

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StudentsComponent } from './components/students/students.component';
+import { StudentDetailsComponent } from './components/student-details/student-details.component';
+import { AddNewStudentComponent } from './components/add-new-student/add-new-student.component';
+import { UpdateStudentComponent } from './components/update-student/update-student.component';
+import { ErrorsComponent } from './components/errors/errors.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:StudentsComponent},
+  {path:'students', component:StudentsComponent},
+  {path:'students/:id', component:StudentDetailsComponent},
+  {path:'addstudent', component:AddNewStudentComponent},
+  {path:'students/:id/edit', component:UpdateStudentComponent},
+  {path:'**', component:ErrorsComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { StudentsComponent } from './components/students/students.component';
+import { StudentDetailsComponent } from './components/student-details/student-details.component';
+import { ErrorsComponent } from './components/errors/errors.component';
+import { AddNewStudentComponent } from './components/add-new-student/add-new-student.component';
+import { UpdateStudentComponent } from './components/update-student/update-student.component';
+import { HttpClientModule} from '@angular/common/http';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    StudentsComponent,
+    StudentDetailsComponent,
+    ErrorsComponent,
+    AddNewStudentComponent,
+    UpdateStudentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
